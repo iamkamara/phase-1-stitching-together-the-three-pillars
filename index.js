8a9d623acb4210526d5805515ae2054266f90695
onesmus <onesmusqamara@gmail.com>
2022-08-29 14:50:34 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 /* This is a file inserted to keep the learn IDE browser happy */
 const testVar = {}
+const glyphStates = {
+    "♡": "♥",
+    "♥": "♡"
+  };
+  
+  const colorStates = {
+    "red" : "",
+    "": "red"
+  };
+  const articleHearts = document.querySelectorAll(".like-glyph");
+
+  function likeCallback(e) {
+    const heart = e.target;
+    mimicServerCall()
+      .then(function(serverMessage){
+        alert("You notified the server!");
+         alert(serverMessage);
+         heart.innerText = glyphStates[heart.innerText];
+         heart.style.color = colorStates[heart.style.color];
+      })
+      .catch(function(error) {
+        alert("Something went wrong!");
+      });
+  }
+  for (const glyph of articleHearts) {
+    glyph.addEventListener("click", likeCallback);
+  } 
+  function mimicServerCall() {
+    return new Promise(function(resolve, reject) {
+      setTimeout(function() {
+        resolve("Pretend remote server notified of action!");
+      }, 300);
+    });
+  } 
